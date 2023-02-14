1d74f110e499786bfbf299ab92833f6cd4e8e80e
iswilljr <iswilljr@gmail.com>
2023-02-13 20:15:16 -0500

    feat: update header navbar links

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,26 +1,48 @@
-import { Link } from "@remix-run/react";
+import { Link, useLocation } from "@remix-run/react";
+import clsx from "clsx";
 import { Logo } from "~/icons/Logo";
 
 export function Header() {
+  const { pathname } = useLocation();
+
+  const isDocsPathname = pathname.startsWith("/docs");
+
   return (
-    <header>
-      <nav className="min-h-[60px] flex justify-between items-center mx-auto w-full px-2 md:px-6">
+    <header
+      className={clsx("bg-white z-10", {
+        sticky: isDocsPathname,
+        "top-0": isDocsPathname,
+      })}
+    >
+      <nav
+        className={clsx(
+          "min-h-[60px] flex justify-between items-center mx-auto w-full px-2 md:px-6 border border-transparent",
+          {
+            "border-b-slate-200": isDocsPathname,
+          }
+        )}
+      >
         <Link to="/">
           <Logo />
         </Link>
-        <ul className="flex justify-center items-center space-x-3 md:space-x-7 text-sm md:text-base">
+        <ul className="flex justify-center items-center space-x-3 md:space-x-7 text-sm md:text-base uppercase">
           <li>
             <Link className="duration-100 hover:text-primary font-bold text-dark-secondary" to="/api" reloadDocument>
               API
             </Link>
           </li>
           <li>
+            <Link className="duration-100 hover:text-primary font-bold text-dark-secondary" to="/docs/introduction">
+              Docs
+            </Link>
+          </li>
+          <li>
             <Link
-              className="duration-100 hover:text-primary font-bold text-dark-secondary uppercase"
-              to="/docs"
+              className="duration-100 hover:text-primary font-bold text-dark-secondary"
+              to="/official"
               reloadDocument
             >
-              Docs
+              Official API
             </Link>
           </li>
         </ul>
