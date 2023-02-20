5817d06704d73d0653bfee58b45890810c3c009e
iswilljr <iswilljr@gmail.com>
2023-02-19 19:33:33 -0500

    chore: update site testing config

--- playwright.config.ts
+++ playwright.config.ts
@@ -52,7 +52,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
+    command: "pnpm remix dev",
     port: 3000,
   },
 });
