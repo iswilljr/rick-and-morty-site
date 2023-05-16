b02c627e5729041eaacdb48d7096429fcd60cf63
iswilljr <iswilljr@gmail.com>
2023-05-16 15:10:13 -0500

    change html element styles

--- src/entry.ssr.tsx
+++ src/entry.ssr.tsx
@@ -8,6 +8,7 @@ export default function (opts: RenderToStreamOptions) {
     ...opts,
     containerAttributes: {
       lang: 'en-us',
+      class: 'overflow-auto',
       ...opts.containerAttributes,
     },
   })
