55fdf2011989c7c8d00ee75343dd0b27fa57baf3
iswilljr <iswilljr@gmail.com>
2023-05-16 12:04:37 -0500

    extended tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#399CD0',
+        secondary: '#9e9e9e',
+        dark: '#202329',
+        'dark-secondary': '#333333',
+        section: '#272b33',
+        'status-alive': '#55cc44',
+        'status-dead': '#d63d2e',
+        'status-unknown': '#9e9e9e',
+      },
+      fontSize: {
+        '8xl': '5.625rem',
+      },
+    },
   },
-  plugins: [],
-};
+  plugins: [require('@tailwindcss/typography')],
+}
