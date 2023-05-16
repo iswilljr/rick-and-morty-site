9710cfd95c82b777bcec8813f246c278584c4ca5
iswilljr <iswilljr@gmail.com>
2023-05-16 11:07:59 -0500

    add tailwindcss integration

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -10,4 +10,5 @@ module.exports = {
   tabWidth: 2,
   trailingComma: 'es5',
   useTabs: false,
+  plugins: [require('prettier-plugin-tailwindcss')],
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,16 @@ import type { DocumentHead } from '@builder.io/qwik-city'
 
 export default component$(() => {
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
+    <main class='grid h-screen w-full place-items-center'>
+      <div>
+        <h1>Hi 👋</h1>
+        <p>
+          Can't wait to see what you build with qwik!
+          <br />
+          Happy coding.
+        </p>
+      </div>
+    </main>
   )
 })
 
