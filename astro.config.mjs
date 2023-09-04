68748bd1179d59cf4f5b89fe2b8558a29c6d6637
iswilljr <iswilljr@gmail.com>
2023-09-03 20:20:25 -0500

    add service worker integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/static'
+import serviceWorker from 'astrojs-service-worker'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://therickandmortyapi.vercel.app',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), serviceWorker()],
   output: 'static',
   adapter: vercel({
     imageService: process.env.VERCEL != null,
