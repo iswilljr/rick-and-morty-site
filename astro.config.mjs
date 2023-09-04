578ba96ddd12375a64fae0be648f72becae9cdcf
iswilljr <iswilljr@gmail.com>
2023-09-03 20:16:28 -0500

    using vercel image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/static'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://therickandmortyapi.vercel.app',
   integrations: [mdx(), sitemap(), tailwind()],
+  output: 'static',
+  adapter: vercel({
+    imageService: process.env.VERCEL != null,
+    imagesConfig: {
+      sizes: [240, 320, 640, 1280],
+      domains: ['raw.githubusercontent.com'],
+    },
+  }),
   markdown: {
     syntaxHighlight: 'prism',
   },
